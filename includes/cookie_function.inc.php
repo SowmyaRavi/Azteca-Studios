28b6d4a0d7c4f81418f840f7a0039242b1501b79
SowmyaRavi <sowmyan5585@gmail.com>
2016-12-12 23:09:32 -0800

    Update cookie_function.inc.php

--- includes/cookie_function.inc.php
+++ includes/cookie_function.inc.php
@@ -21,22 +21,7 @@ function page_counter($cookie_name,$path){
       return $msg;
 }
 
-function getRealIpAddr()
-{
-    if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
-    {
-      $ip=$_SERVER['HTTP_CLIENT_IP'];
-    }
-    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
-    {
-      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
-    }
-    else
-    {
-      $ip=$_SERVER['REMOTE_ADDR'];
-    }
-    return $ip;
-}
+
 
 function del_sess_if_exp(){
         //subtract new timestamp from the old one
