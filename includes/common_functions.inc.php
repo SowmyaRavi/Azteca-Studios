d102c26c9d0c2e806213acc16ef50a5707ac5d53
SowmyaRavi <sowmyan5585@gmail.com>
2016-12-12 23:04:52 -0800

    Update common_functions.inc.php

--- includes/common_functions.inc.php
+++ includes/common_functions.inc.php
@@ -153,37 +153,7 @@ function get_page_no($dbh,$q,$display){
     return $pages;    
 }
 
-function link_section($pages,$start,$display, $sort,$base_url){
-    // Make the links to other pages, if necessary.
-    if ($pages > 1) {
 
-            echo '<br/><p>';
-            $current_page = ($start/$display) + 1;
-
-            // If it's not the first page, make a Previous button:
-            if ($current_page != 1) {
-                    echo '<a href=' . $base_url . '?s=' . ($start - $display) . '&p=' . $pages . '&sort=' . $sort . '>Previous</a> ';
-            }
-
-            // Make all the numbered pages:
-            for ($i = 1; $i <= $pages; $i++) {
-                    if ($i != $current_page) {
-                            echo '<a href=' . $base_url . '?s=' . (($display * ($i - 1))) . '&p=' . $pages . '&sort=' . $sort . '> '. $i .' </a>';
-                    } else {
-                            echo $i . ' ';
-                    }
-            } // End of FOR loop.
-
-            // If it's not the last page, make a Next button:
-            if ($current_page != $pages) {
-                    echo '<a href=' . $base_url . '?s=' . ($start + $display) . '&p=' . $pages . '&sort=' . $sort . '>Next</a>';
-            }
-
-            echo '</p>'; // Close the paragraph.
-
-    } // End of links section.
-    
-}
 
 
 
